0e8356908226851cb60d2f845f926f905c5828c1
monij <monij@DESKTOP-4MC4PHB.home>
2020-04-15 16:58:23 -0700

    Searching and sorting algorithms(Finished)

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -44,10 +44,51 @@ public class Algorithms {
 	
 	public static boolean containsSOS (List<String> morse) {
 		for (int i = 0; i < morse.size(); i++) {
-			if (morse.get(i) == "... --- ...") {
+			System.out.println(morse.get(i));
+			if (morse.get(i).equals(" ... --- ... ")) {
 				return true;
 			}
 		}
 		return false;
 	}
+
+	public static List<Double> sortScores(List<Double> results) {
+		for (int i = 0; i < results.size(); i++) {
+			for (int j = 0; j < results.size()-1; j++) {
+				if (results.get(j)>results.get(j+1)) {
+					double tempNum = results.get(j);
+					results.set(j, results.get(j+1));
+					results.set(j+1, tempNum);
+				}
+			}
+		}
+		return results;
+	}
+
+	public static List<String> sortDNA(List<String> unsortedSequences) {
+		for (int i = 0; i < unsortedSequences.size(); i++) {
+			for (int j = 0; j < unsortedSequences.size()-1; j++) {
+				if (unsortedSequences.get(j).length()>unsortedSequences.get(j+1).length()) {
+					String tempNum = unsortedSequences.get(j);
+					unsortedSequences.set(j, unsortedSequences.get(j+1));
+					unsortedSequences.set(j+1, tempNum);
+				}
+			}
+		}
+		return unsortedSequences;
+	}
+
+	public static List<String> sortWords(List<String> words) {
+		for (int i = 0; i < words.size(); i++) {
+			for (int j = 0; j < words.size()-1; j++) {
+				if (words.get(j).compareTo(words.get(j+1)) > 0) {
+					String tempNum = words.get(j);
+					words.set(j, words.get(j+1));
+					words.set(j+1, tempNum);
+				}
+			}
+		}
+		return words;
+	}
+	
 }
